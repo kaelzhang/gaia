3da6222b8bb36225b063f7dfbe6ac7c577004f4f
Kael Zhang <i+github@kael.me>
2018-06-14 17:00:20 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'lib/index.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    commonjs({
+      include: ['node_modules/**']
+    }),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+}
---
+++ test/gaea.js
@@ -0,0 +1,6 @@
+import test from 'ava'
+import gaea from '../src'
+
+test('description', t => {
+  t.is(true, true)
+})
