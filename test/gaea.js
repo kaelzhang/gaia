e9f31e71e9fccbbf614ec8edd5c09d6b6dc39232
Kael Zhang <i+github@kael.me>
2018-06-29 14:35:28 +0800

    tests

--- test/gaea.js
+++ test/gaea.js
@@ -1,5 +1,27 @@
 const {test} = require('piapia')
+const hello = require('../example/hello')
+const {server, client} = hello
 
-test('description', t => {
-  return Promise.resolve()
+let Greeter
+
+test.before = () => {
+  server.start()
+
+  const {
+    helloworld
+  } = client('localhost:50051')
+
+  Greeter = helloworld.Greeter
+}
+
+test.after = () => {
+  process.nextTick(() => {
+    process.exit()
+  })
+}
+
+test('sayHello', async t => {
+  const {message} = await Greeter.sayHello({name: 'world'})
+
+  t.is(message, 'Hello world')
 })
